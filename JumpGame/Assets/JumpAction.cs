b760f65800c4906419c87323db66f7f2abe7ac21
pennyyi97 <pennyyi97@gmail.com>
2024-12-02 12:19:45 +0900

    점프 게임

---
+++ JumpGame/Assets/JumpAction.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class JumpAction : MonoBehaviour
+{
+    Vector3 forward = new Vector3 (0, 0, 1);
+    Vector3 up = new Vector3(0,1,0);
+
+    void Update()
+    {
+      float v = Input.GetAxis("Vertical");
+      float h = Input.GetAxis("Horizontal");
+
+      v = v * Time.deltaTime;
+      h = h * Time.deltaTime;
+
+      transform.Translate(v * forward * 20);
+      transform.Rotate(h * up * 20);
+
+      if (Input.GetButton("Jump")) {
+        GetComponent<Rigidbody>().AddForce(new Vector3(0, 20, 0));
+      }
+    }
+}
